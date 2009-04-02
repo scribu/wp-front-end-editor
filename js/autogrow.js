93b03e37230255d7307566065d02b4b684d218d1
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-02 18:34:50 +0000

    autogrow fixes
    git-svn-id: http://plugins.svn.wordpress.org/front-end-editor/trunk@107254 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/autogrow.js
+++ js/autogrow.js
@@ -1,5 +1,5 @@
-/* 
- * Auto Expanding Text Area (1.2.2)
+/*
+ * Auto Expanding Text Area (1.2.4)
  * by Chrys Bader (www.chrysbader.com)
  * chrysb@gmail.com
  *
@@ -8,35 +8,35 @@
  * John Resig - jeresig@gmail.com
  *
  * Copyright (c) 2008 Chrys Bader (www.chrysbader.com)
- * Licensed under the GPL (GPL-LICENSE.txt) license. 
+ * Licensed under the GPL (GPL-LICENSE.txt) license.
  *
  *
  * NOTE: This script requires jQuery to work.  Download jQuery at www.jquery.com
  *
  */
- 
+
 (function(jQuery) {
-		  
+		
 	var self = null;
- 
+
 	jQuery.fn.autogrow = function(o)
-	{	
+	{
 		return this.each(function() {
 			new jQuery.autogrow(this, o);
 		});
 	};
-	
 
-    /**
-     * The autogrow object.
-     *
-     * @constructor
-     * @name jQuery.autogrow
-     * @param Object e The textarea to create the autogrow for.
-     * @param Hash o A set of key/value pairs to set as configuration properties.
-     * @cat Plugins/autogrow
-     */
-	
+
+	/**
+	 * The autogrow object.
+	 *
+	 * @constructor
+	 * @name jQuery.autogrow
+	 * @param Object e The textarea to create the autogrow for.
+	 * @param Hash o A set of key/value pairs to set as configuration properties.
+	 * @cat Plugins/autogrow
+	 */
+
 	jQuery.autogrow = function (e, o)
 	{
 		this.options		  	= o || {};
@@ -46,87 +46,107 @@
 		this.min_height		  	= this.options.minHeight || parseInt(jQuery(e).css('min-height'));
 		this.max_height		  	= this.options.maxHeight || parseInt(jQuery(e).css('max-height'));;
 		this.textarea		  	= jQuery(e);
-		
-		if(this.line_height == NaN)
-		  this.line_height = 0;
-		
+
+		if (isNaN(this.line_height))
+		{
+		this.line_height = 0;
+		}
+
+		if (isNaN(this.min_height) || this.min_height == 0)
+		{
+			this.min_height == this.textarea.height();
+		}
+
 		// Only one textarea activated at a time, the one being used
 		this.init();
-	};
-	
+	}
+
 	jQuery.autogrow.fn = jQuery.autogrow.prototype = {
-    autogrow: '1.2.2'
-  };
-	
+		autogrow: '1.2.4'
+  	}
+
  	jQuery.autogrow.fn.extend = jQuery.autogrow.extend = jQuery.extend;
-	
+
 	jQuery.autogrow.fn.extend({
-						 
-		init: function() {	
-			var self = this;			
+						
+		init: function() {
+			var self = this;
 			this.textarea.css({overflow: 'hidden', display: 'block'});
 			this.textarea.bind('focus', function() { self.startExpand() } ).bind('blur', function() { self.stopExpand() });
-			this.checkExpand();	
+			this.checkExpand(0, true);
 		},
-						 
-		startExpand: function() {				
-		  var self = this;
-			this.interval = window.setInterval(function() {self.checkExpand()}, 400);
+
+		startExpand: function() {
+		  	var self = this;
+			// while focused, leave an extra line.  The cursor can never actually reach
+			// this extra line, but it makes the gesture for select-all much easier to perform
+			// (or, _possible_ to perform, if the last line is overflow-x-hidden)
+			this.checkExpand(this.line_height, true);
+			this.interval = window.setInterval(function() {self.checkExpand(self.line_height, false)}, 400);
 		},
-		
+
 		stopExpand: function() {
-			clearInterval(this.interval);	
+			clearInterval(this.interval);
+			this.checkExpand(0, true);
 		},
-		
-		checkExpand: function() {
-		    
+
+		checkExpand: function(extraspace, forcecheck) {
+
 			if (this.dummy == null)
 			{
 				this.dummy = jQuery('<div></div>');
 				this.dummy.css({
-												'font-size'  : this.textarea.css('font-size'),
-												'font-family': this.textarea.css('font-family'),
-												'width'      : this.textarea.css('width'),
-												'padding'    : this.textarea.css('padding'),
-												'line-height': this.line_height + 'px',
-												'overflow-x' : 'hidden',
-												'position'   : 'absolute',
-												'top'        : 0,
-												'left'		 : -9999
-												}).appendTo('body');
+					'font-size'  : this.textarea.css('font-size'),
+					'font-family': this.textarea.css('font-family'),
+					'width'	  : this.textarea.css('width'),
+					'padding'	: this.textarea.css('padding'),
+					'line-height': this.line_height + 'px',
+					'overflow-x' : 'hidden',
+					'position'   : 'absolute',
+					'top'		: 0,
+					'left'		 : -9999
+				}).appendTo('body');
 			}
-			
-			// Strip HTML tags
-			var html = this.textarea.val().replace(/(<|>)/g, '');
-			
+
+			// textarea -> div
+			// textarea rules are unique. it's not the same as <pre> because it word-wraps,
+			// but it does not condense spaces like html.
+			// dot at the end is to make sure we leave room for the cursor, if the cursor is on a blank line
+			var html = this.textarea.val().toString()
+				.replace(/&/g, '&amp;')
+				.replace(/</g, '&lt;')
+				.replace(/>/g, '&gt;')
+				.replace(/^ /, '&nbsp;')
+				.replace(/  /g, ' &nbsp;') + ".";
+
 			// IE is different, as per usual
-			if (jQuery.browser.msie)
+			if (jQuery.support.leadingWhitespace)
 			{
-				html = html.replace(/\n/g, '<BR>new');
+				html = html.replace(/\n/g, '<br>');
 			}
 			else
 			{
-				html = html.replace(/\n/g, '<br>new');
+				html = html.replace(/\n/g, '<BR>');
 			}
-			
-			if (this.dummy.html() != html)
+
+			if (forcecheck || this.dummy.html() != html)
 			{
-				this.dummy.html(html);	
-				
-				if (this.max_height > 0 && (this.dummy.height() + this.line_height > this.max_height))
+				this.dummy.html(html);
+
+				if (this.max_height > 0 && (this.dummy.height() + extraspace + 1 > this.max_height))
 				{
-					this.textarea.css('overflow-y', 'auto');	
+					this.textarea.height(this.max_height);
+					this.textarea.css('overflow-y', 'auto');
 				}
 				else
 				{
 					this.textarea.css('overflow-y', 'hidden');
-					if (this.textarea.height() < this.dummy.height() + this.line_height || (this.dummy.height() < this.textarea.height()))
-					{	
-						this.textarea.animate({height: (this.dummy.height() + this.line_height) + 'px'}, 100);	
+					if (this.textarea.height() != this.dummy.height() + extraspace + 1)
+					{
+						this.textarea.animate({height: (this.dummy.height() + extraspace + 1) + 'px'}, 100);
 					}
 				}
 			}
-		}
-						 
+		}			
 	 });
 })(jQuery);
