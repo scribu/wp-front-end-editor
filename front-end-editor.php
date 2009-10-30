463e0175f4955bc475c2b439dba06398642536fd
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-30 08:36:01 +0000

    up version
    git-svn-id: http://plugins.svn.wordpress.org/front-end-editor/trunk@168633 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- front-end-editor.php
+++ front-end-editor.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Front-end Editor
-Version: 1.4a3
+Version: 1.4a4
 Description: Allows you to edit your posts without going through the admin interface
 Author: scribu
 Author URI: http://scribu.net/
