be905ceaf10d98bf0fa4b14d1383a176c02b236a
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-18 10:33:47 +0000

    bump version again
    git-svn-id: http://plugins.svn.wordpress.org/front-end-editor/trunk@254179 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- front-end-editor.php
+++ front-end-editor.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Front-end Editor
-Version: 1.9-alpha2
+Version: 1.9-alpha3
 Description: Allows you to edit your posts without going through the admin interface
 Author: scribu
 Author URI: http://scribu.net/
@@ -57,7 +57,7 @@ function _fee_init() {
 		),
 	));
 
-	FEE_Core::init($options, '1.9-alpha2');
+	FEE_Core::init($options, '1.9-alpha3');
 
 	FEE_Field_Option::init(__FILE__);
 	FEE_Field_Image::init(__FILE__);
