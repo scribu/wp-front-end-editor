868887dff47cbb36f14de4c55be6e81dc0f4a703
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-28 00:17:58 +0000

    Fix incompatibilities with Proper Network Activation
    git-svn-id: http://plugins.svn.wordpress.org/front-end-editor/trunk@281759 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- front-end-editor.php
+++ front-end-editor.php
@@ -35,10 +35,10 @@ function _fee_init() {
 	$dir = dirname( __FILE__ );
 
 	// Load files
-	require $dir . '/core.php';
+	require_once $dir . '/core.php';
 
 	foreach ( array( 'base', 'post', 'other' ) as $name )
-		require "$dir/fields/$name.php";
+		require_once "$dir/fields/$name.php";
 
 	$options = new scbOptions( 'front-end-editor', __FILE__, array(
 		'disabled' => array(),
@@ -63,129 +63,128 @@ function _fee_init() {
 	FEE_Field_Image::init( __FILE__ );
 
 	if ( is_admin() ) {
-		require $dir . '/admin.php';
+		require_once $dir . '/admin.php';
 		scbAdminPage::register( 'Fee_Admin', __FILE__, $options );
 	}
 
-	function fee_register_defaults() {
-		$fields = array(
-			'the_title' => array(
-				'title' => __( 'Post title', 'front-end-editor' ),
-				'class' => 'FEE_Field_Post',
-				'type'  => 'input',
-			),
-
-			'the_content' => array(
-				'title' => __( 'Post content', 'front-end-editor' ),
-				'class' => FEE_Core::$options->chunks ? 'FEE_Field_Chunks' : 'FEE_Field_Post',
-				'type'  => FEE_Core::$options->rich ? 'rich' : 'textarea',
-			),
-
-			'the_excerpt' => array(
-				'title' => __( 'Post excerpt', 'front-end-editor' ),
-				'class' => 'FEE_Field_Excerpt',
-				'type'  => 'textarea',
-			),
-
-			'the_category' => array(
-				'title' => __( 'Post categories', 'front-end-editor' ),
-				'class' => 'FEE_Field_Category',
-				'type'  => 'terminput',
-				'argc'  => 3,
-			),
-
-			'the_tags' => array(
-				'title' => __( 'Post tags', 'front-end-editor' ),
-				'class' => 'FEE_Field_Tags',
-				'type'  => 'terminput',
-				'argc'  => 4,
-			),
-
-			'the_terms' => array(
-				'title' => __( 'Post terms', 'front-end-editor' ),
-				'class' => 'FEE_Field_Terms',
-				'type'  => 'terminput',
-				'argc'  => 5,
-			),
-
-			'post_meta' => array(
-				'title' => __( 'Post custom fields', 'front-end-editor' ),
-				'class' => 'FEE_Field_Meta',
-				'argc'  => 5,
-			),
-
-			'post_thumbnail_html' => array(
-				'title' => __( 'Post thumbnail', 'front-end-editor' ),
-				'class' => 'FEE_Field_Thumbnail',
-				'type'  => 'thumbnail',
-				'argc'  => 4,
-			),
-
-			'comment_text' => array(
-				'title' => __( 'Comment text', 'front-end-editor' ),
-				'class' => 'FEE_Field_Comment',
-				'type'  => 'textarea',
-			),
-
-			'single_cat_title' => array(
-				'title' => __( 'Category title', 'front-end-editor' ),
-				'class' => 'FEE_Field_Single_Title',
-			),
-
-			'single_tag_title' => array(
-				'title' => __( 'Tag title', 'front-end-editor' ),
-				'class' => 'FEE_Field_Single_Title',
-			),
-
-			'term_description' => array(
-				'title' => __( 'Term description', 'front-end-editor' ),
-				'class' => 'FEE_Field_Term_Field',
-				'type'  => 'textarea',
-				'argc'  => 3,
-			),
-
-			'the_author_description' => array(
-				'title' => __( 'Author description', 'front-end-editor' ),
-				'class' => 'FEE_Field_Author_Desc',
-				'type'  => 'textarea',
-				'argc'  => 2,
-			),
-
-			'dynamic_sidebar_params' => array(
-				'title' => __( 'Widgets', 'front-end-editor' ),
-				'class' => 'FEE_Field_Widget',
-				'type'  => 'widget',
-			),
-
-			'bloginfo' => array(
-				'title' => __( 'Site title and description', 'front-end-editor' ),
-				'class' => 'FEE_Field_Bloginfo',
-				'argc'  => 2,
-			),
-
-			'editable_option' => array(
-				'title' => __( 'Site options', 'front-end-editor' ),
-				'class' => 'FEE_Field_Option',
-				'argc'  => 3,
-			),
-
-			'editable_image' => array(
-				'title' => __( 'Theme images', 'front-end-editor' ),
-				'class' => 'FEE_Field_Image',
-				'type'  => 'image',
-				'argc'  => 2,
-			),
-		);
-
-		foreach ( $fields as $filter => $args )
-			register_fronted_field( $filter, $args );
-
-		// Safe hook for new editable fields to be registered
-		do_action( 'front_end_editor_fields' );
-	}
-
 	add_action( 'init', 'fee_register_defaults' );
-
 }
 scb_init( '_fee_init' );
+
+function fee_register_defaults() {
+	$fields = array(
+		'the_title' => array(
+			'title' => __( 'Post title', 'front-end-editor' ),
+			'class' => 'FEE_Field_Post',
+			'type'  => 'input',
+		),
+
+		'the_content' => array(
+			'title' => __( 'Post content', 'front-end-editor' ),
+			'class' => FEE_Core::$options->chunks ? 'FEE_Field_Chunks' : 'FEE_Field_Post',
+			'type'  => FEE_Core::$options->rich ? 'rich' : 'textarea',
+		),
+
+		'the_excerpt' => array(
+			'title' => __( 'Post excerpt', 'front-end-editor' ),
+			'class' => 'FEE_Field_Excerpt',
+			'type'  => 'textarea',
+		),
+
+		'the_category' => array(
+			'title' => __( 'Post categories', 'front-end-editor' ),
+			'class' => 'FEE_Field_Category',
+			'type'  => 'terminput',
+			'argc'  => 3,
+		),
+
+		'the_tags' => array(
+			'title' => __( 'Post tags', 'front-end-editor' ),
+			'class' => 'FEE_Field_Tags',
+			'type'  => 'terminput',
+			'argc'  => 4,
+		),
+
+		'the_terms' => array(
+			'title' => __( 'Post terms', 'front-end-editor' ),
+			'class' => 'FEE_Field_Terms',
+			'type'  => 'terminput',
+			'argc'  => 5,
+		),
+
+		'post_meta' => array(
+			'title' => __( 'Post custom fields', 'front-end-editor' ),
+			'class' => 'FEE_Field_Meta',
+			'argc'  => 5,
+		),
+
+		'post_thumbnail_html' => array(
+			'title' => __( 'Post thumbnail', 'front-end-editor' ),
+			'class' => 'FEE_Field_Thumbnail',
+			'type'  => 'thumbnail',
+			'argc'  => 4,
+		),
+
+		'comment_text' => array(
+			'title' => __( 'Comment text', 'front-end-editor' ),
+			'class' => 'FEE_Field_Comment',
+			'type'  => 'textarea',
+		),
+
+		'single_cat_title' => array(
+			'title' => __( 'Category title', 'front-end-editor' ),
+			'class' => 'FEE_Field_Single_Title',
+		),
+
+		'single_tag_title' => array(
+			'title' => __( 'Tag title', 'front-end-editor' ),
+			'class' => 'FEE_Field_Single_Title',
+		),
+
+		'term_description' => array(
+			'title' => __( 'Term description', 'front-end-editor' ),
+			'class' => 'FEE_Field_Term_Field',
+			'type'  => 'textarea',
+			'argc'  => 3,
+		),
+
+		'the_author_description' => array(
+			'title' => __( 'Author description', 'front-end-editor' ),
+			'class' => 'FEE_Field_Author_Desc',
+			'type'  => 'textarea',
+			'argc'  => 2,
+		),
+
+		'dynamic_sidebar_params' => array(
+			'title' => __( 'Widgets', 'front-end-editor' ),
+			'class' => 'FEE_Field_Widget',
+			'type'  => 'widget',
+		),
+
+		'bloginfo' => array(
+			'title' => __( 'Site title and description', 'front-end-editor' ),
+			'class' => 'FEE_Field_Bloginfo',
+			'argc'  => 2,
+		),
+
+		'editable_option' => array(
+			'title' => __( 'Site options', 'front-end-editor' ),
+			'class' => 'FEE_Field_Option',
+			'argc'  => 3,
+		),
+
+		'editable_image' => array(
+			'title' => __( 'Theme images', 'front-end-editor' ),
+			'class' => 'FEE_Field_Image',
+			'type'  => 'image',
+			'argc'  => 2,
+		),
+	);
+
+	foreach ( $fields as $filter => $args )
+		register_fronted_field( $filter, $args );
+
+	// Safe hook for new editable fields to be registered
+	do_action( 'front_end_editor_fields' );
+}
 
