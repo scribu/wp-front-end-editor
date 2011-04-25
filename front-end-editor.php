364fde033edb1891ddf989b07ce7230c76b96883
scribu <mail@scribu.net>
2011-04-25 14:44:55 +0300

    bump script version

--- front-end-editor.php
+++ front-end-editor.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Front-end Editor
-Version: 2.0-alpha
+Version: 2.0-alpha1
 Description: Allows you to edit your posts without going through the admin interface
 Author: scribu
 Author URI: http://scribu.net/
@@ -54,7 +54,7 @@ function _fee_init() {
 			"source"
 	) );
 
-	FEE_Core::init( $options, '2.0-alpha' );
+	FEE_Core::init( $options, '2.0-alpha1' );
 
 	FEE_Field_Option::init( __FILE__ );
 	FEE_Field_Image::init( __FILE__ );
