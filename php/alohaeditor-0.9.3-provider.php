94a36fa2918e67a0654d9c35df1300379fa5c704
scribu <mail@scribu.net>
2011-05-27 00:50:53 +0300

    aloha-provider.php cleanup: * use WP bundled jQuery * add 'aloha-' prefix to dependency names * enqueue link plugin * delete aloha-config.js (didn't do anything useful)

--- php/alohaeditor-0.9.3-provider.php
+++ php/alohaeditor-0.9.3-provider.php
@@ -12,63 +12,37 @@ abstract class FEE_AlohaEditor {
 	 * of the admin area (dashboard).
 	 */
 	static function enqueueAloha() {
-
-		//wp_register_script('aewip',  'js/aewip.js', array (), '0.10', false);
-		wp_register_script('aloha.config', plugins_url('js/alohaeditor-config.js', FRONT_END_EDITOR_MAIN_FILE), array (), '0.9.3', false);
-
-		// Deregister jquery and register aloha jquery version
-		wp_deregister_script('jquery');
-		wp_register_script('jquery', FEE_AlohaEditor::_getAlohaSrcBaseUrl() . 'deps/jquery-1.4.4.js', array (), '0.9.3', false);
-
 		if (is_user_logged_in() && !is_admin()) {
-			// Load format plugin and all aloha dependencies
-			wp_enqueue_script('aloha.format');
-			wp_enqueue_script('aloha.table');
-			wp_enqueue_script('aloha.list');
-			wp_enqueue_script('aloha.fee');
-			wp_enqueue_script('aloha.imagewp');
+			wp_enqueue_script('aloha-plugin-format');
+			wp_enqueue_script('aloha-plugin-link');
+			wp_enqueue_script('aloha-plugin-list');
+			wp_enqueue_script('aloha-plugin-table');
+			wp_enqueue_script('aloha-plugin-fee');
+			wp_enqueue_script('aloha-plugin-imagewp');
+
 			if (defined('ALOHA_FEE_EXPERIMENTAL')) {
-				wp_enqueue_script('aloha.image');
-				wp_enqueue_script('aloha.draganddropfiles');
+				wp_enqueue_script('aloha-plugin-image');
+				wp_enqueue_script('aloha-plugin-draganddropfiles');
 			}
-
-			// Load the aloha editor configuration
-			wp_enqueue_script('aloha.config');
 		}
-	}
-
-	/**
-	 * Returns the absolute baseurl to the aloha editor plugin directory
-	 */
-	static function _getAlohaPluginsBaseUrl() {
-		return self::_getAlohaSrcBaseUrl() . 'plugins/';
-	}
-
-	/**
-	 * Returns the absolute baseurl to the aloha editor src directory
-	 */
-	static function _getAlohaSrcBaseUrl() {
-		return plugins_url('aloha-editor/WebContent/', FRONT_END_EDITOR_MAIN_FILE);
 	}
 
 	/**
 	 * Registers the aloha editor depdencies and plugins
 	 */
 	static function registerAloha() {
-
-		$alohaSrcBaseUrl = self::_getAlohaSrcBaseUrl();
-		$alohaPluginsBaseUrl = self::_getAlohaPluginsBaseUrl();
+		$alohaSrcBaseUrl = plugins_url('aloha-editor/WebContent/', FRONT_END_EDITOR_MAIN_FILE);
+		$alohaPluginsBaseUrl = plugins_url('aloha-editor/WebContent/plugins/', FRONT_END_EDITOR_MAIN_FILE);
 		$alohaCustomPluginsBaseUrl = plugins_url('aloha-plugins/', FRONT_END_EDITOR_MAIN_FILE);
 
 		//Include no deps version for development
 		if (defined('SCRIPT_DEBUG')) {
 
 		// Aloha Deps
-		wp_register_script('jquery', $alohaSrcBaseUrl . 'deps/jquery-1.4.4.js', array (), '0.9.3', false);
-		wp_register_script('jquery.json', $alohaSrcBaseUrl . 'deps/jquery.json-2.2.min.js', array (), '0.9.3', false);
-		wp_register_script('jquery.getUrlParam', $alohaSrcBaseUrl . 'deps/jquery.getUrlParam.js', array (), '0.9.3', false);
-		wp_register_script('jquery.prettyPhoto', $alohaSrcBaseUrl . 'deps/prettyPhoto/jquery.prettyPhoto.js', array (), '0.9.3', false);
-		wp_register_script('jquery.cookie', $alohaSrcBaseUrl . 'deps/jquery.cookie.js', array (), '0.9.3', false);
+		wp_register_script('jquery-json', $alohaSrcBaseUrl . 'deps/jquery.json-2.2.min.js', array (), '0.9.3', false);
+		wp_register_script('jquery-getUrlParam', $alohaSrcBaseUrl . 'deps/jquery.getUrlParam.js', array (), '0.9.3', false);
+		wp_register_script('jquery-prettyPhoto', $alohaSrcBaseUrl . 'deps/prettyPhoto/jquery.prettyPhoto.js', array (), '0.9.3', false);
+		wp_register_script('jquery-cookie', $alohaSrcBaseUrl . 'deps/jquery.cookie.js', array (), '0.9.3', false);
 		wp_register_script('ext-jquery-adapter-debug', $alohaSrcBaseUrl . 'deps/extjs/ext-jquery-adapter-debug.js', array (), '0.9.3', false);
 		wp_register_script('ext-foundation-debug', $alohaSrcBaseUrl . 'deps/extjs/ext-foundation-debug.js', array (), '0.9.3', false);
 		wp_register_script('cmp-foundation-debug', $alohaSrcBaseUrl . 'deps/extjs/cmp-foundation-debug.js', array (), '0.9.3', false);
@@ -89,27 +63,27 @@ abstract class FEE_AlohaEditor {
 		wp_register_script('pkg-forms-debug', $alohaSrcBaseUrl . 'deps/extjs/pkg-forms-debug.js', array (), '0.9.3', false);
 
 		// Aloha JQuery Deps
-		wp_register_script('jquery.aloha.ext', $alohaSrcBaseUrl .  'utils/jquery.js', array (), '0.9.3', false);
+		wp_register_script('jquery-aloha.ext', $alohaSrcBaseUrl .  'utils/jquery.js', array (), '0.9.3', false);
 
 		// Other deps
-		wp_register_script('lang', $alohaSrcBaseUrl . 'utils/lang.js', array (), '0.9.3', false);
-		wp_register_script('range', $alohaSrcBaseUrl . 'utils/range.js', array (), '0.9.3', false);
-		wp_register_script('position', $alohaSrcBaseUrl . 'utils/position.js', array (), '0.9.3', false);
-		wp_register_script('dom', $alohaSrcBaseUrl . 'utils/dom.js', array (), '0.9.3', false);
-		wp_register_script('indexof', $alohaSrcBaseUrl . 'utils/indexof.js', array (), '0.9.3', false);
-		wp_register_script('license', $alohaSrcBaseUrl . 'core/license.js', array (), '0.9.3', false);
+		wp_register_script('aloha-lang', $alohaSrcBaseUrl . 'utils/lang.js', array (), '0.9.3', false);
+		wp_register_script('aloha-range', $alohaSrcBaseUrl . 'utils/range.js', array (), '0.9.3', false);
+		wp_register_script('aloha-position', $alohaSrcBaseUrl . 'utils/position.js', array (), '0.9.3', false);
+		wp_register_script('aloha-dom', $alohaSrcBaseUrl . 'utils/dom.js', array (), '0.9.3', false);
+		wp_register_script('aloha-indexof', $alohaSrcBaseUrl . 'utils/indexof.js', array (), '0.9.3', false);
+		wp_register_script('aloha-license', $alohaSrcBaseUrl . 'core/license.js', array (), '0.9.3', false);
 		wp_register_script('ext-alohaproxy', $alohaSrcBaseUrl . 'core/ext-alohaproxy.js', array (), '0.9.3', false);
 		wp_register_script('ext-alohareader',$alohaSrcBaseUrl . 'core/ext-alohareader.js', array (), '0.9.3', false);
 		wp_register_script('ext-alohatreeloader',$alohaSrcBaseUrl.'core/ext-alohatreeloader.js', array (), '0.9.3', false);
 
 
 		// register core dependencies
-		wp_register_script('core', $alohaSrcBaseUrl . 'core/core.js', array (
+		wp_register_script('aloha-core', $alohaSrcBaseUrl . 'core/core.js', array (
 			'jquery',
-			'jquery.json',
-			'jquery.getUrlParam',
-			'jquery.prettyPhoto',
-			'jquery.cookie',
+			'jquery-json',
+			'jquery-getUrlParam',
+			'jquery-prettyPhoto',
+			'jquery-cookie',
 			'ext-jquery-adapter-debug',
 			'ext-foundation-debug',
 			'cmp-foundation-debug',
@@ -127,88 +101,87 @@ abstract class FEE_AlohaEditor {
 			'pkg-toolbars-debug',
 			'pkg-menu-debug',
 			'pkg-forms-debug',
-			'jquery.aloha.ext',
-			'lang',
-			'range',
-			'position',
-			'dom',
-			'indexof',
-			'license',
+			'jquery-aloha.ext',
+			'aloha-lang',
+			'aloha-range',
+			'aloha-position',
+			'aloha-dom',
+			'aloha-indexof',
+			'aloha-license',
 			'ext-alohaproxy',
 			'ext-alohareader',
 			'ext-alohatreeloader'
 		), '0.9.3', false);
 
 
-
 		// register ui scripts
-		wp_register_script('ui', $alohaSrcBaseUrl . 'core/ui.js', array (), '0.9.3', false);
-		wp_register_script('ui-attributefield', $alohaSrcBaseUrl . 'core/ui-attributefield.js', array (), '0.9.3', false);
-		wp_register_script('ui-browser', $alohaSrcBaseUrl . 'core/ui-browser.js', array (), '0.9.3', false);
-		wp_register_script('css', $alohaSrcBaseUrl . 'core/css.js', array (), '0.9.3', false);
-		wp_register_script('editable', $alohaSrcBaseUrl . 'core/editable.js', array (), '0.9.3', false);
-		wp_register_script('ribbon', $alohaSrcBaseUrl . 'core/ribbon.js', array (), '0.9.3', false);
-		wp_register_script('event', $alohaSrcBaseUrl . 'core/event.js', array (), '0.9.3', false);
-		wp_register_script('floatingmenu', $alohaSrcBaseUrl . 'core/floatingmenu.js', array (), '0.9.3', false);
-		wp_register_script('ierange-m2', $alohaSrcBaseUrl . 'core/ierange-m2.js', array (), '0.9.3', false);
-		wp_register_script('jquery.aloha', $alohaSrcBaseUrl . 'core/jquery.aloha.js', array (), '0.9.3', false);
-		wp_register_script('log', $alohaSrcBaseUrl . 'core/log.js', array (), '0.9.3', false);
-		wp_register_script('markup', $alohaSrcBaseUrl . 'core/markup.js', array (), '0.9.3', false);
-		wp_register_script('message', $alohaSrcBaseUrl . 'core/message.js', array (), '0.9.3', false);
-		wp_register_script('plugin', $alohaSrcBaseUrl . 'core/plugin.js', array (), '0.9.3', false);
-		wp_register_script('selection', $alohaSrcBaseUrl . 'core/selection.js', array (), '0.9.3', false);
-		wp_register_script('sidebar', $alohaSrcBaseUrl . 'core/sidebar.js', array (), '0.9.3', false);
-		wp_register_script('repositorymanager', $alohaSrcBaseUrl . 'core/repositorymanager.js', array (), '0.9.3', false);
-		wp_register_script('repository', $alohaSrcBaseUrl . 'core/repository.js', array (), '0.9.3', false);
-		wp_register_script('repositoryobjects', $alohaSrcBaseUrl . 'core/repositoryobjects.js', array (), '0.9.3', false);
+		wp_register_script('aloha-ui', $alohaSrcBaseUrl . 'core/ui.js', array (), '0.9.3', false);
+		wp_register_script('aloha-ui-attributefield', $alohaSrcBaseUrl . 'core/ui-attributefield.js', array (), '0.9.3', false);
+		wp_register_script('aloha-ui-browser', $alohaSrcBaseUrl . 'core/ui-browser.js', array (), '0.9.3', false);
+		wp_register_script('aloha-css', $alohaSrcBaseUrl . 'core/css.js', array (), '0.9.3', false);
+		wp_register_script('aloha-editable', $alohaSrcBaseUrl . 'core/editable.js', array (), '0.9.3', false);
+		wp_register_script('aloha-ribbon', $alohaSrcBaseUrl . 'core/ribbon.js', array (), '0.9.3', false);
+		wp_register_script('aloha-event', $alohaSrcBaseUrl . 'core/event.js', array (), '0.9.3', false);
+		wp_register_script('aloha-floatingmenu', $alohaSrcBaseUrl . 'core/floatingmenu.js', array (), '0.9.3', false);
+		wp_register_script('aloha-ierange-m2', $alohaSrcBaseUrl . 'core/ierange-m2.js', array (), '0.9.3', false);
+		wp_register_script('aloha-jquery.aloha', $alohaSrcBaseUrl . 'core/jquery.aloha.js', array (), '0.9.3', false);
+		wp_register_script('aloha-log', $alohaSrcBaseUrl . 'core/log.js', array (), '0.9.3', false);
+		wp_register_script('aloha-markup', $alohaSrcBaseUrl . 'core/markup.js', array (), '0.9.3', false);
+		wp_register_script('aloha-message', $alohaSrcBaseUrl . 'core/message.js', array (), '0.9.3', false);
+		wp_register_script('aloha-plugin', $alohaSrcBaseUrl . 'core/plugin.js', array (), '0.9.3', false);
+		wp_register_script('aloha-selection', $alohaSrcBaseUrl . 'core/selection.js', array (), '0.9.3', false);
+		wp_register_script('aloha-sidebar', $alohaSrcBaseUrl . 'core/sidebar.js', array (), '0.9.3', false);
+		wp_register_script('aloha-repositorymanager', $alohaSrcBaseUrl . 'core/repositorymanager.js', array (), '0.9.3', false);
+		wp_register_script('aloha-repository', $alohaSrcBaseUrl . 'core/repository.js', array (), '0.9.3', false);
+		wp_register_script('aloha-repositoryobjects', $alohaSrcBaseUrl . 'core/repositoryobjects.js', array (), '0.9.3', false);
 
 		$plugindeps = array (
-			'core',
-			'ui',
-			'ui-attributefield',
-			'ui-browser',
-			'css',
-			'editable',
-			'ribbon',
-			'event',
-			'floatingmenu',
-			'ierange-m2',
-			'jquery.aloha',
-			'log',
-			'markup',
-			'message',
-			'plugin',
-			'selection',
-			'sidebar',
-			'repositorymanager',
-			'repository',
-			'repositoryobjects'
+			'aloha-core',
+			'aloha-ui',
+			'aloha-ui-attributefield',
+			'aloha-ui-browser',
+			'aloha-css',
+			'aloha-editable',
+			'aloha-ribbon',
+			'aloha-event',
+			'aloha-floatingmenu',
+			'aloha-ierange-m2',
+			'aloha-jquery.aloha',
+			'aloha-log',
+			'aloha-markup',
+			'aloha-message',
+			'aloha-plugin',
+			'aloha-selection',
+			'aloha-sidebar',
+			'aloha-repositorymanager',
+			'aloha-repository',
+			'aloha-repositoryobjects'
 		);
 
-		// Aloha Plugins
-		wp_register_script('aloha.format', $alohaPluginsBaseUrl . 'com.gentics.aloha.plugins.Format/plugin.js', $plugindeps, '0.9.3', false);
-		wp_register_script('aloha.table', $alohaPluginsBaseUrl . 'com.gentics.aloha.plugins.Table/plugin.js', $plugindeps, '0.9.3', false);
-		wp_register_script('aloha.list',  $alohaPluginsBaseUrl . 'com.gentics.aloha.plugins.List/plugin.js', $plugindeps, '0.9.3', false);
-		wp_register_script('aloha.link', $alohaPluginsBaseUrl . 'com.gentics.aloha.plugins.Link/plugin.js', $plugindeps, '0.9.3', false);
-		wp_register_script('aloha.highlighteditables', $alohaPluginsBaseUrl . 'com.gentics.aloha.plugins.HighlightEditables/plugin.js', $plugindeps, '0.9.3', false);
-		wp_register_script('aloha.TOC', $alohaPluginsBaseUrl .'com.gentics.aloha.plugins.TOC/plugin.js' ,$plugindeps, '0.9.3', false);
-		wp_register_script('aloha.delicious', $alohaPluginsBaseUrl .'com.gentics.aloha.plugins.Link/delicious.js', $plugindeps, '0.9.3', false);
-		wp_register_script('aloha.link', $alohaPluginsBaseUrl .'com.gentics.aloha.plugins.Link/LinkList.js', $plugindeps, '0.9.3', false);
-		wp_register_script('aloha.paste', $alohaPluginsBaseUrl . 'com.gentics.aloha.plugins.Paste/plugin.js', $plugindeps, '0.9.3', false);
-		wp_register_script('aloha.wordpastehandler', $alohaPluginsBaseUrl .'com.gentics.aloha.plugins.Paste/wordpastehandler.js', $plugindeps, '0.9.3', false);
-		wp_register_script('aloha.fee', $alohaCustomPluginsBaseUrl. 'com.gentics.aloha.plugins.FEE/plugin.js',$plugindeps,'0.9.3',false);
-		wp_register_script('aloha.imagewp', $alohaCustomPluginsBaseUrl. 'com.gentics.aloha.plugins.ImageWP/plugin.js',$plugindeps,'0.9.3',false);
+
+		// register plugins
+		wp_register_script('aloha-plugin-format', $alohaPluginsBaseUrl . 'com.gentics.aloha.plugins.Format/plugin.js', $plugindeps, '0.9.3', false);
+		wp_register_script('aloha-plugin-table', $alohaPluginsBaseUrl . 'com.gentics.aloha.plugins.Table/plugin.js', $plugindeps, '0.9.3', false);
+		wp_register_script('aloha-plugin-list',  $alohaPluginsBaseUrl . 'com.gentics.aloha.plugins.List/plugin.js', $plugindeps, '0.9.3', false);
+		wp_register_script('aloha-plugin-link', $alohaPluginsBaseUrl . 'com.gentics.aloha.plugins.Link/plugin.js', $plugindeps, '0.9.3', false);
+		wp_register_script('aloha-plugin-highlighteditables', $alohaPluginsBaseUrl . 'com.gentics.aloha.plugins.HighlightEditables/plugin.js', $plugindeps, '0.9.3', false);
+		wp_register_script('aloha-plugin-TOC', $alohaPluginsBaseUrl .'com.gentics.aloha.plugins.TOC/plugin.js' ,$plugindeps, '0.9.3', false);
+		wp_register_script('aloha-plugin-delicious', $alohaPluginsBaseUrl .'com.gentics.aloha.plugins.Link/delicious.js', $plugindeps, '0.9.3', false);
+		wp_register_script('aloha-plugin-link', $alohaPluginsBaseUrl .'com.gentics.aloha.plugins.Link/LinkList.js', $plugindeps, '0.9.3', false);
+		wp_register_script('aloha-plugin-paste', $alohaPluginsBaseUrl . 'com.gentics.aloha.plugins.Paste/plugin.js', $plugindeps, '0.9.3', false);
+		wp_register_script('aloha-plugin-wordpastehandler', $alohaPluginsBaseUrl .'com.gentics.aloha.plugins.Paste/wordpastehandler.js', $plugindeps, '0.9.3', false);
+		wp_register_script('aloha-plugin-fee', $alohaCustomPluginsBaseUrl. 'com.gentics.aloha.plugins.FEE/plugin.js',$plugindeps,'0.9.3',false);
+		wp_register_script('aloha-plugin-imagewp', $alohaCustomPluginsBaseUrl. 'com.gentics.aloha.plugins.ImageWP/plugin.js',$plugindeps,'0.9.3',false);
 
 		// Image Plugin
-		wp_register_script('aloha.image', $alohaCustomPluginsBaseUrl . 'com.gentics.aloha.plugins.image/plugin.js',$plugindeps,'0.9.3',false);
+		wp_register_script('aloha-plugin-image', $alohaCustomPluginsBaseUrl . 'com.gentics.aloha.plugins.image/plugin.js',$plugindeps,'0.9.3',false);
 		// Drag and Drop plugin
-		wp_register_script('aloha.draganddropfiles.xhruploader', $alohaCustomPluginsBaseUrl . 'com.gentics.aloha.plugins.DragAndDropFiles/deps/Ext.ux.XHRUpload.js',array(),'0.9.3',false);
-		//wp_register_script('aloha.draganddropfiles.uploader', $alohaCustomPluginsBaseUrl . 'com.gentics.aloha.plugins.DragAndDropFiles/lib/uploader.js',array(),'0.9.3',false);
-		wp_register_script('aloha.draganddropfiles.repository', $alohaCustomPluginsBaseUrl . 'com.gentics.aloha.plugins.DragAndDropFiles/lib/DropFilesRepository.js',array(),'0.9.3',false);
+		wp_register_script('aloha-plugin-draganddropfiles.xhruploader', $alohaCustomPluginsBaseUrl . 'com.gentics.aloha.plugins.DragAndDropFiles/deps/Ext.ux.XHRUpload.js',array(),'0.9.3',false);
+		//wp_register_script('aloha-plugin-draganddropfiles.uploader', $alohaCustomPluginsBaseUrl . 'com.gentics.aloha.plugins.DragAndDropFiles/lib/uploader.js',array(),'0.9.3',false);
+		wp_register_script('aloha-plugin-draganddropfiles.repository', $alohaCustomPluginsBaseUrl . 'com.gentics.aloha.plugins.DragAndDropFiles/lib/DropFilesRepository.js',array(),'0.9.3',false);
 
-		$dragAndDropDependencies = array('aloha.draganddropfiles.xhruploader','aloha.draganddropfiles.repository','aloha.image');
-		//array_push($dragAndDropDependencies,$plugindeps);
-		wp_register_script('aloha.draganddropfiles', $alohaCustomPluginsBaseUrl . 'com.gentics.aloha.plugins.DragAndDropFiles/plugin.js',$dragAndDropDependencies,'0.9.3',false);
+		$dragAndDropDependencies = array('aloha-plugin-draganddropfiles.xhruploader','aloha.draganddropfiles.repository','aloha.image');
+		wp_register_script('aloha-plugin-draganddropfiles', $alohaCustomPluginsBaseUrl . 'com.gentics.aloha.plugins.DragAndDropFiles/plugin.js',$dragAndDropDependencies,'0.9.3',false);
 		} else {
 			//TODO decide whether we should use the build version
 		}
