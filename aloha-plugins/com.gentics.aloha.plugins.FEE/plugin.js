3af7badc318c69c81a99e3be45a9428e6f4708c3
scribu <mail@scribu.net>
2011-05-28 22:54:37 +0300

    icons it is

--- aloha-plugins/com.gentics.aloha.plugins.FEE/plugin.js
+++ aloha-plugins/com.gentics.aloha.plugins.FEE/plugin.js
@@ -6,11 +6,21 @@ GENTICS.Aloha.FEE = jQuery.extend( new GENTICS.Aloha.Plugin('com.gentics.aloha.p
 	init: function () {
 		// create the Save button
 		var save_button = new GENTICS.Aloha.ui.Button({
+			'iconClass' : 'GENTICS_button Example_strong',
+			'size' : 'small',
 			'onclick' : jQuery.proxy(this, 'save'),
-			'label' : FrontEndEditor.data.save_text
+			'tooltip' : FrontEndEditor.data.save_text
 		});
 
-		// add it to the floating menu
+		// create the Cancel button
+		var cancel_button = new GENTICS.Aloha.ui.Button({
+			'iconClass' : 'GENTICS_button Example_strong',
+			'size' : 'small',
+			'onclick' : jQuery.proxy(this, 'cancel'),
+			'tooltip' : FrontEndEditor.data.cancel_text
+		});
+
+		// add buttons to the floating menu
 		GENTICS.Aloha.FloatingMenu.addButton(
 			'GENTICS.Aloha.continuoustext',
 			save_button,
@@ -18,13 +28,6 @@ GENTICS.Aloha.FEE = jQuery.extend( new GENTICS.Aloha.Plugin('com.gentics.aloha.p
 			4
 		);
 
-		// create the Cancel button
-		var cancel_button = new GENTICS.Aloha.ui.Button({
-			'onclick' : jQuery.proxy(this, 'cancel'),
-			'label' : FrontEndEditor.data.cancel_text
-		});
-
-		// add it to the floating menu
 		GENTICS.Aloha.FloatingMenu.addButton(
 			'GENTICS.Aloha.continuoustext',
 			cancel_button,
