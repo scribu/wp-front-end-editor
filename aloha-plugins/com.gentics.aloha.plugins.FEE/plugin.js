a7339c3bbacb7aac5880a24abf6e5d6d743396d4
scribu <mail@scribu.net>
2011-05-28 22:48:45 +0300

    add cancel button to Aloha

--- aloha-plugins/com.gentics.aloha.plugins.FEE/plugin.js
+++ aloha-plugins/com.gentics.aloha.plugins.FEE/plugin.js
@@ -1,52 +1,43 @@
 /**
- *
+ * Plugin that adds the Save and cancel buttons
  */
-GENTICS.Aloha.Fee = new GENTICS.Aloha.Plugin('com.gentics.aloha.plugins.Fee');
+GENTICS.Aloha.FEE = jQuery.extend( new GENTICS.Aloha.Plugin('com.gentics.aloha.plugins.FEE'), {
 
+	init: function () {
+		// create the Save button
+		var save_button = new GENTICS.Aloha.ui.Button({
+			'onclick' : jQuery.proxy(this, 'save'),
+			'label' : FrontEndEditor.data.save_text
+		});
 
-//GENTICS.Aloha.Aewip.languages = ['en', 'de'];
+		// add it to the floating menu
+		GENTICS.Aloha.FloatingMenu.addButton(
+			'GENTICS.Aloha.continuoustext',
+			save_button,
+			GENTICS.Aloha.i18n(GENTICS.Aloha, 'floatingmenu.tab.format'),
+			4
+		);
 
+		// create the Cancel button
+		var cancel_button = new GENTICS.Aloha.ui.Button({
+			'onclick' : jQuery.proxy(this, 'cancel'),
+			'label' : FrontEndEditor.data.cancel_text
+		});
 
-GENTICS.Aloha.Fee.scopes = { "aloha":0, "markdown":1 };
+		// add it to the floating menu
+		GENTICS.Aloha.FloatingMenu.addButton(
+			'GENTICS.Aloha.continuoustext',
+			cancel_button,
+			GENTICS.Aloha.i18n(GENTICS.Aloha, 'floatingmenu.tab.format'),
+			4
+		);
+	},
 
-/**
-* Default scope is aloha
-*/
-GENTICS.Aloha.Fee.scope = GENTICS.Aloha.Fee.scopes['aloha'];
+	save: function() {
+		alert('save');
+	},
 
-GENTICS.Aloha.Fee.init = function () {
-	var that = this;
-
-	//GENTICS.Aloha.Ribbon.hide();
-
-	//GENTICS.Aloha.Ribbon.show();
-
-	// create a new button
-	  var that = this;
-
-	  var button = new GENTICS.Aloha.ui.Button({
-	    'iconClass' : 'GENTICS_button Example_strong',
-	    'size' : 'small',
-	    'onclick' : function () {
-	                  that.save();
-	            },
-	    'tooltip' : that.i18n('button.save.tooltip')
-	  });
-
-	  // add it to the floating menu
-	  GENTICS.Aloha.FloatingMenu.addButton(
-	    'GENTICS.Aloha.continuoustext',
-	    button,
-	    GENTICS.Aloha.i18n(GENTICS.Aloha, 'floatingmenu.tab.format'),
-	    4
-	  );
-	
-}
-
-GENTICS.Aloha.Fee.save = function() {
-	alert('save');
-}
-
-
-
-
+	cancel: function() {
+		alert('cancel');
+	}
+});
