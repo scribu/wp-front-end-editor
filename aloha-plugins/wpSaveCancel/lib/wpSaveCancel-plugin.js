a07956586a483f4ccd760dcfb492551ab7f4b207
scribu <mail@scribu.net>
2011-12-10 20:13:16 +0200

    convert wpSaveCancel plugin to Coco. see #48

--- aloha-plugins/wpSaveCancel/lib/wpSaveCancel-plugin.js
+++ aloha-plugins/wpSaveCancel/lib/wpSaveCancel-plugin.js
@@ -1,68 +1,33 @@
-/**
- * Plugin that adds the Save and cancel buttons
- */
-define([
-	// js
-	'aloha/jquery',
-	'aloha/plugin',
-	'aloha/floatingmenu',
-	'i18n!aloha/nls/i18n',
-	'i18n!wpSaveCancel/nls/i18n'
-],
-function WPImagePlugin ( aQuery, Plugin, FloatingMenu, i18nCore, i18n) {
-	
-	var jQuery = aQuery;
-	var $ = aQuery;
-	var GENTICS = window.GENTICS, Aloha = window.Aloha;
-
-	return Plugin.create('org.fee.plugins.wpSaveCancel', {
-
-		init: function () {
-		// create the Save button
-		this.saveButton = new Aloha.ui.Button({
-			'iconClass' : 'SaveFEE',
-			'size' : 'small',
-			'onclick' : jQuery.proxy(this, 'save'),
-			'tooltip' : FrontEndEditor.data.save_text
-		});
-
-		// create the Cancel button
-		this.cancelButton = new Aloha.ui.Button({
-			'iconClass' : 'CancelFEE',
-			'size' : 'small',
-			'onclick' : jQuery.proxy(this, 'cancel'),
-			'tooltip' : FrontEndEditor.data.cancel_text
-		});
-
-		// add buttons to the floating menu
-		FloatingMenu.addButton(
-			'GENTICS.Aloha.continuoustext',
-			this.saveButton,
-			i18nCore.t('floatingmenu.tab.format'),
-			4
-		);
-
-		FloatingMenu.addButton(
-			'GENTICS.Aloha.continuoustext',
-			this.cancelButton,
-			i18nCore.t('floatingmenu.tab.format'),
-			4
-		);
-	},
-
-	save: function() {
-		this.current_field.ajax_set();
-
-		this.disableAloha();
-	},
-
-	cancel: function() {
-		this.current_field.remove_form();
-
-		this.disableAloha();
-	},
-
-	disableAloha: function() {
-		Aloha.deactivateEditable()
-	}
+define(['aloha/plugin', 'aloha/floatingmenu', 'i18n!aloha/nls/i18n'], function(Plugin, FloatingMenu, i18nCore){
+  var Aloha;
+  Aloha = window.Aloha;
+  return Plugin.create('org.fee.plugins.wpSaveCancel', {
+    init: function(){
+      this.saveButton = new Aloha.ui.Button({
+        'iconClass': 'SaveFEE',
+        'size': 'small',
+        'onclick': jQuery.proxy(this, 'save'),
+        'tooltip': FrontEndEditor.data.save_text
+      });
+      this.cancelButton = new Aloha.ui.Button({
+        'iconClass': 'CancelFEE',
+        'size': 'small',
+        'onclick': jQuery.proxy(this, 'cancel'),
+        'tooltip': FrontEndEditor.data.cancel_text
+      });
+      FloatingMenu.addButton('GENTICS.Aloha.continuoustext', this.saveButton, i18nCore.t('floatingmenu.tab.format'), 4);
+      return FloatingMenu.addButton('GENTICS.Aloha.continuoustext', this.cancelButton, i18nCore.t('floatingmenu.tab.format'), 4);
+    },
+    save: function(){
+      this.current_field.ajax_set();
+      return this.disableAloha();
+    },
+    cancel: function(){
+      this.current_field.remove_form();
+      return this.disableAloha();
+    },
+    disableAloha: function(){
+      return Aloha.deactivateEditable();
+    }
+  });
 });
