53d9af2f5097e17571d3972ae86c3198f59fd3ec
scribu <mail@scribu.net>
2011-12-28 19:24:33 +0200

    update to Aloha 0.20.0 stable

--- front-end-editor.php
+++ front-end-editor.php
@@ -27,7 +27,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 define( 'FEE_VERSION', '2.2.1-alpha' );
-define( 'ALOHA_VERSION', '0.20.0-RC9' );
+define( 'ALOHA_VERSION', '0.20.0' );
 
 define( 'FEE_MAIN_FILE', __FILE__ );
 
