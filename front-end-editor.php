72707dd37d93c655464d221ecee1cf11d3c97482
scribu <mail@scribu.net>
2012-04-30 18:48:38 +0300

    replace copyright notice with 'License:' header

--- front-end-editor.php
+++ front-end-editor.php
@@ -8,22 +8,8 @@ Author URI: http://scribu.net/
 Plugin URI: http://scribu.net/wordpress/front-end-editor
 Text Domain: front-end-editor
 Domain Path: /lang
-
-
-Copyright (C) 2009-2012 Cristi Burcă (mail@scribu.net)
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.txt
 */
 
 define( 'FEE_VERSION', '2.3-alpha3' );
